188c48e5296021f33d0636ac1412969e71f79988
Orhan Gazi Kılıç <orhngazi@gmail.com>
2024-10-01 00:59:05 +0300

    Update SummaryTr.js
    Son düzeltmeleri ekledim.

--- src/components/SummaryTr.js
+++ src/components/SummaryTr.js
@@ -4,14 +4,17 @@ const SummaryTr = () => {
 			<h6>Hakkımda:</h6>
 			<p>
 				2010 yılından 2012 yılında mezun olana kadar üniversitemde kendi bölümümde kısmi süreli çalışan öğrenci olarak çalıştım.
-				2010 yılında Erzincan Üniversite'sini kazandıktan bu yana yazılım geliştirme faaliyetlerinde bulundum. 2012 yılında Anadolu Üniversitesi İşletme Fakültesinden mezun oldum.
+				2010 yılında Erzincan Üniversite'sini kazandıktan bu yana yazılım geliştirme faaliyetlerinde bulundum. 2014 yılında Anadolu Üniversitesi İşletme Fakültesinden mezun oldum.
 			</p>
 			<p>
-				2017-2018 yılları arasında Gazi Yazılım şirketini kurup yönettim, 2017 Ağustos ayından 2019 Eylül ayına kadar Beyoğlu
+				2017 yılına kadar freelancer olarak çalıştım. 2017-2018 yılları arasında Gazi Yazılım şirketini kurup yönettim, 2017 Ağustos ayından 2019 Eylül ayına kadar Beyoğlu
 				Kaza Danışmanlık şirketinin kurumsal erp-crm-hrm yazılımının geliştirilmesinde yazılım uzmanı olarak çalıştım. 
 			</p>
 			<p>
-				<strong>Php, mysql, sqlite on mobile, html, css, javascript, jquery, react ve react-native, redux, java ve spring framework'ü uzman seviyesinde bilmekteyim. Restful kullanarak ve yazılım tasarım kalıplarını kullanarak yazılım geliştirmekteyim.</strong> Çalıştığım projeler ve ayrıntılar için linkedin'im aşağıda:
+				<strong>8 yıl php, mysql, html, css, javascript ve jquery ile web uygulamaları geliştirdim</strong>
+			</p>
+			<p>
+				<strong>5 yıldır java ve spring framework, mysql, react ve react-native, redux, sqlite on mobile ve javascript ile web uygulamaları, android ve ios uygulamaları geliştirmekteyim. Restful kullanarak ve yazılım tasarım kalıplarını kullanarak yazılım geliştirmekteyim.</strong> Çalıştığım projeler ve ayrıntılar için linkedin'im aşağıda:
 			</p>
 		</div>
 	)
